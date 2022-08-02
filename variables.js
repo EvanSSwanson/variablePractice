973c39774bbc7df27309799d13d12ccfd487828e
Evan Swanson <evan.sundelius@gmail.com>
2022-08-02 14:13:11 -0500

    Update variables

--- variables.js
+++ variables.js
@@ -1,9 +1,15 @@
-var city = Chicago
-var county = Cook
-var state = Illinois
+var city = "Chicago"
+var county = "Cook"
+var state = "Illinois"
 var stateFounding = 1818
 var amountBaseballTeams = 2
 var popRanking = 3
 var tooHumid = true
 var tooCold = true
 var politiciansGood = false
+var city = "Honolulu"
+var county = "Oahu"
+var state = "Hawaii"
+console.log(city)
+console.log(county)
+console.log(state)
